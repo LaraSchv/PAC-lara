3f05a554cf92d547e8afa46847851c6704ab79cd
LaraSchv <lara.schv@gmail.com>
2022-09-17 12:46:21 -0300

    ajustes sobre la logica de limpieza del df

---
+++ configs.py
@@ -0,0 +1,38 @@
+lista_nombres_nuevos = [
+        'marca temporal',
+        'nombre de usuario', 
+        'choclo',
+        'palta',
+        'tomate perita',
+        'morron verde',
+        'uva',
+        'berenjena agroecologica',
+        'ajo',
+        'ciruela',
+        'manzana roja',
+        'limon',
+        'pera',
+        'naranja',
+        'durazno',
+        'morron rojo',
+        'zapallito redondo',
+        'tomate cherry',
+        'mandarina',
+        'frutillas',
+        'banana',
+        'mango',
+        'arandanos',
+        'jengibre',
+        'retirar'
+]
+
+dict_nombres_nuevos = {
+        'bolson de verduras verdes': 'bolson de verdes',
+        'bolson de pesadas': 'bolson de pesadas',
+        'combo bolson': 'combo bolson verdes pesadas',
+        'combo citrico': 'bolson citricos'
+}
+
+lista_columnas_necesarias = lista_nombres_nuevos + list(dict_nombres_nuevos.values())
+
+barrios = ['Palermo', 'Villa Urquiza', 'Villa Crespo']
